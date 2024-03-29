ff9abdcab6a46048f4eebf94c4a0f226cc14bb26
thuanhm.spkt@gmail.com <thuanhm.spkt@gmail.com>
2024-03-29 22:02:20 +0700

    res

--- asset/js/javascript.js
+++ asset/js/javascript.js
@@ -6,10 +6,10 @@ function toggleContent(contentId) {
     document.getElementById(currentContent).style.display = 'none'; // Ẩn nội dung hiện tại
     if(contentId === 'flashSale')
     {
-      document.getElementById(contentId).style.display = 'block'; // Hiển thị nội dung mới
+      document.getElementById(contentId).style.display = 'inline-block'; // Hiển thị nội dung mới
     }
     else{
-      document.getElementById(contentId).style.display = 'flex'; // Hiển thị nội dung mới
+      document.getElementById(contentId).style.display = 'inline-block'; // Hiển thị nội dung mới
     }
     peviousContent = currentContent; // dùng để khi nhấn qua nội dung hiện tại thì nội dung trước đó được add lại css phần dấu gạch dưới có hiệu ứng
     currentContent = contentId; // Cập nhật nội dung hiện tại
@@ -26,3 +26,19 @@ function toggleContent(contentId) {
       document.getElementById(peviousContent + '-header').classList.add("underline-hover")
   }
 }
+
+// Bắt sự kiện khi click vào nút menu
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  document.getElementById("menu").classList.toggle("active");
+});
+
+// Bắt sự kiện khi click vào bất kỳ nơi nào trên trang
+document.addEventListener("click", function(event) {
+  var menu = document.getElementById("menu");
+  var menuToggle = document.getElementById("menu-toggle");
+
+  // Kiểm tra xem click có xảy ra bên ngoài menu và nút menu-toggle hay không
+  if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+      menu.classList.remove("active"); // Nếu có, đóng menu
+  }
+});
